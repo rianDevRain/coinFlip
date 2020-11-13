cd002fb5b1dfa529b2a861823f1f2818b40f307f
Rian <mail@rian.dev>
2020-11-13 11:34:12 -0600

    Initial Commit

---
+++ script/script.js
@@ -0,0 +1,47 @@
+/** @format */
+
+const coin = {
+	state: 0,
+	flip() {
+		this.state = Math.round(Math.random());
+	},
+	toString() {
+		if (this.state === 0) {
+			return "Tails";
+		} else {
+			return "Heads";
+		}
+	},
+	toHTML() {
+		let image = document.createElement("img");
+		if (this.state === 0) {
+			return "./images/tails.jpg";
+		} else {
+			return "images/heads.jpg";
+		}
+		return image;
+	},
+};
+
+
+
+function display20Flips() {
+	for (let i = 0; i < 19; i++) {
+		coin.flip();
+		// image.src = "./images";
+		let paragraph = document.createElement("p");
+		paragraph.innerHTML = coin.toString();
+		document.body.append(paragraph);
+			}
+}
+display20Flips();
+
+function display20Images() {
+	for (let i = 0; i < 19; i++) {
+		coin.flip();
+		let img = document.createElement("img");
+		img.src = coin.toHTML();
+		document.body.append(img);
+	}
+}
+display20Images();
